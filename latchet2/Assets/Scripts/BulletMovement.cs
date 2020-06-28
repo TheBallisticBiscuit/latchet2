d57f1473ade46372474f33ef65893c53ec94a84a
Jeremy Bost <jeremybost@outlook.com>
2020-06-28 12:56:49 -0700

    Very basic red bullet firing on click

---
+++ latchet2/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float timeToLive = 3f;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToLive -= Time.deltaTime;
+
+        if (timeToLive <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
