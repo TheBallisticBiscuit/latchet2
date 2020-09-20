7a393334e94ceca7de54ebc8b4e373bb0467ff6b
Noah Newell <newellnd022@gmail.com>
2020-09-20 16:47:52 -0400

    Adding Test Dummy, making bullets do damage

---
+++ latchet2/Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damage = 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health hpScript = other.gameObject.GetComponent<Health>();
+        if (hpScript != null)
+        {
+            hpScript.TakeDamage(damage);
+        }
+    }
+}
---
+++ latchet2/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxHP = 100;
+    private int hp;
+    private bool dead = false;
+    void Start()
+    {
+        hp = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0 && !dead)
+        {
+            Debug.Log(this.gameObject.name + " Is Dead");            
+            dead = true;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+    }
+}
